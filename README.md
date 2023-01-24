3db13590e4f74cc583bc6b3cc6efdb964440d23a
Melissa Gillen <melissagillen@Melissas-MacBook-Pro.local>
2023-01-23 21:58:23 -0500

    made some updates

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# READMEGenerator
+{"Title":"Yyy","Description":"y","TableOfContents":true,"Installation":"y","Usage":true,"Credits":true,"License":[],"Contribute":true,"Test":true,"Questions":true}
--- index.js
+++ index.js
@@ -1,66 +1,74 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require("fs");
-inquirer
-.createPromptModule(questions)
-.then(response)=>
-// TODO: Create an array of questions for user input
+const generateMarkdown = require("./utils/generateMarkdown");
+
 const questions = [ 
-    {
-      type: 'input',
-      message: 'What is your Projects Title?',
-      name: 'Title',
-    },
-    {
-        type: "input",
-        message:"Please provide a short description. What problem is beign solve, why did you build this, what did you gain? ",
-        name:"Description",
+  {
+    type: 'input',
+    message: 'What is your Projects Title?',
+    name: 'Title',
+  },
+  {
+      type: "input",
+      message:"Please provide a short description. What problem is beign solve, why did you build this, what did you gain? ",
+      name:"Description",
 
-    },
-    {
-        type:"confirm",
-        message:"Is a table of contents needed?",
-        name:"TableOfContents",
-    },
-    {
-      type: 'input',
-      message: 'What is needed for installation?',
-      name: 'Installation',
-    },
-    {
-      type: 'confirm',
-      message: 'Do you have instructions or Example to show?',
-      name: 'Usage',
-    },
-    {
-        type:"confirm",
-        message:"Do you need to add collaborators?",
-        name: "Credits"
-    },
-    {
-        type:"checkbox",
-        message: "Which liscense do you need to include",
-        choices:["MIT","Microsoft Public License","Eclipse Public License 2.0","Mozilla Public License 2.0","Open Software License 3.0"],
-        name:"License",
-    },
-    {
-        type:"confirm",
-        message:"How to contribute?",
-        name:"Contribute",
-    },
-    {
-            type:"confirm",
-            message:"Do you want to provide test?",
-            name: "Test"
-        },
-    {
-        type:"confirm",
-        message:"Do you want to provide a questions section?",
-        name:"Questions"
-    },
+  },
+  {
+      type:"confirm",
+      message:"Is a table of contents needed?",
+      name:"TableOfContents",
+  },
+  {
+    type: 'input',
+    message: 'What is needed for installation?',
+    name: 'Installation',
+  },
+  {
+    type: 'confirm',
+    message: 'Do you have instructions or Example to show?',
+    name: 'Usage',
+  },
+  {
+      type:"confirm",
+      message:"Do you need to add collaborators?",
+      name: "Credits"
+  },
+  {
+      type:"checkbox",
+      message: "Which liscense do you need to include",
+      choices:["MIT","Microsoft Public License","Eclipse Public License 2.0","Mozilla Public License 2.0","Open Software License 3.0"],
+      name:"License",
+  },
+  {
+      type:"confirm",
+      message:"How to contribute?",
+      name:"Contribute",
+  },
+  {
+          type:"confirm",
+          message:"Do you want to provide test?",
+          name: "Test"
+      },
+  {
+      type:"confirm",
+      message:"Do you want to provide a questions section?",
+      name:"Questions"
+  },
 
-  ]
+]
+inquirer
+.prompt(questions)
+.then(answers =>{
+  console.log(answers);
+  fs.writeFile('README.md',JSON.stringify(answers), function(err){
+    if (err) throw err;
+  console.log("saved")
+      });
+// TODO: Create an array of questions for user input
 
+})
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
